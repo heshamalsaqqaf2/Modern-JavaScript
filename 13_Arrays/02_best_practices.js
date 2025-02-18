b78e64be2064c2da4ed856239ca8e329907f6f86
Hesham Alsaqqaf <heshamalsaqqaf14@gmail.com>
2025-02-19 02:42:04 +0300

    Upload Best Practices Arrays.

---
+++ 13_Arrays/02_best_practices.js
@@ -0,0 +1,18 @@
+/**
+ * @Best_Practices
+ *    1- Choose the appropriate loop for your use case.
+ *    2- Always ensure your loop has a way to terminate to avoid infinite loops
+ *    3- Keep your loops as simple as possible for readability.
+ *    4- Use meaningful variable names for your loop counters.
+ *    5- Be cautious when modifying loop variables within the loop body.
+ *
+ * @summary
+ *    1- The for loop is used when we know the number of iterations in advance.
+ *    2- The while loop continues as long as a condition is true.
+ *    3- The do...while loop always executes at least once before checking the condition.
+ *    4- The for...of loop provides a simple way to iterate over iterable objects like strings.
+ *    5- Be cautious of infinite loops, which can occur when the loop condition is always true.
+ *    6- break and continue can be used to control the flow within loops.
+ *
+ *  *In the next section, we'll explore loops in JavaScript.
+*/
