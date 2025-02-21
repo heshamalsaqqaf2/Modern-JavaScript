42d84944ffdb85f46018306500713929f5e4af7b
Hesham Alsaqqaf <heshamalsaqqaf14@gmail.com>
2025-02-21 04:40:28 +0300

    Upload Best Practices Objects.

---
+++ 14_Objects/07_Best_Practices_and_Summary/01_best_practices.js
@@ -0,0 +1,20 @@
+/**
+ * @Best_Practices
+ *    1- Use consistent property access methods (prefer dot notation when possible)
+ *    2- Check for property existence before accessing values
+ *    3- Use meaningful property names
+ *    4- Use Object destructuring when working with multiple properties
+ *    5- Be careful with object mutations (changes)
+ *
+ * @summary
+ *    1- Store related data using key-value pairs
+ *    2- Access and modify properties using dot or bracket notation
+ *    3- Check for property existence using various methods
+ *    4- Iterate over properties using for...in or Object methods
+ *    5- Extract properties using destructuring
+ *    6- Collect remaining properties using the rest operator
+ *
+ *  *Understanding objects is crucial for JavaScript development as they are fundamental,
+ *  *to how data is structured and manipulated in the language.
+ *  
+*/
