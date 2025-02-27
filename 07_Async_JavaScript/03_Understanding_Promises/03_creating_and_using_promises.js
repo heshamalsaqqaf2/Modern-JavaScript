fc8aa2c3aac8ec8d1194a33d19d51b87dbf56a64
Hesham Alsaqqaf <heshamalsaqqaf14@gmail.com>
2025-02-28 02:16:46 +0300

    Add Promise Asynchronous Concept.

--- 07_Async_JavaScript/03_Understanding_Promises/03_creating_and_using_promises.js
+++ 07_Async_JavaScript/03_Understanding_Promises/03_creating_and_using_promises.js
@@ -3,21 +3,40 @@
 // Let's create a more practical example - simulating a database operation:
 function getUser(id) {
     return new Promise((resolve, reject) => {
-        console.log('Starting...');
+        console.log(`🔃 Fetching users ${id}...`);
 
         setTimeout(() => {
-            if (Math.random() > 0.5) {
-                const user = { id: id, name: 'Hesham' };
+            // Simulate random success/failure
+            if (Math.random() > 0.1) { // 90% success rate
+                const user = {
+                    id: id,
+                    name: 'Hesham',
+                    age: 25,
+                    email: 'heshsm@gmail.com',
+                    address: {
+                        country: 'Yemen',
+                        city: 'Taiz',
+                        street: '2025-Street',
+                    }
+                };
                 resolve(user);
+
             } else {
                 reject(new Error('Failed to fetch users.'));
             }
         }, 2000);
-    }).then((user) => {
-        console.log('Success: ', user);
-    }).catch((err) => {
-        console.log('Error: ', err.message);
     });
 }
 
-getUser(1);
+
+getUser(1)
+    .then((user) => {
+        console.log('✅ Success Fetching Operation : ', user);
+    }).catch((err) => {
+        console.log('❌ Error Fetching Operation: ', err.message);
+    }).finally(() => {
+        console.log('Operation complete: [ success or failure ]')
+    });
+
+console.log(`Process Operation 2...`);
+console.log(`Process Operation 3...`);
