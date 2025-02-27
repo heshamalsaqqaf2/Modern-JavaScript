5377cfdbab543781bfdc0c2fb94f1ab3f372a9da
Hesham Alsaqqaf <heshamalsaqqaf14@gmail.com>
2025-02-28 00:24:42 +0300

    Add Asynchronous Concept.

---
+++ 07_Async_JavaScript/02_Understanding_Callbacks/Summary.js
@@ -0,0 +1,10 @@
+/** 
+ * @summary
+ *     1- Callbacks are functions passed to other functions to be executed later
+ *     2- They're used heavily in asynchronous programming
+ *     3- We've been using them already (array methods)
+ *     4- They can lead to complex nested code ("callback hell")
+ *     5- They're still important to understand even though we have better tools now
+ * 
+ *  * Next, we'll learn about Promises, which provide a more elegant way to handle asynchronous operations.
+*/
