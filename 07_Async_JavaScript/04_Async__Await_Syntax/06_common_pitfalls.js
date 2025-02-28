542f88231632db796efefb4feba00735b1a7b38a
Hesham Alsaqqaf <heshamalsaqqaf14@gmail.com>
2025-03-01 01:05:09 +0300

    Add Async/Await Concept.

---
+++ 07_Async_JavaScript/04_Async__Await_Syntax/06_common_pitfalls.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// TODO: -------------------------- Forgetting await: --------------------------
+// Wrong
+async function example() {
+    const result = getData();  // !Oops! Forgot await
+    console.log(result);  // !Logs Promise object, not the data
+}
+// Correct
+async function example() {
+    const result = await getData();
+    console.log(result);  // Logs the actual data
+}
+
+// TODO: -------------------------- await in a regular function: --------------------------
+// Wrong
+function example() {
+    // const data = await getData();  // !Error: await only valid in async function
+}
+// Correct
+async function example() {
+    const data = await getData();
+}
+
+// TODO: -------------------------- Not handling errors: --------------------------
