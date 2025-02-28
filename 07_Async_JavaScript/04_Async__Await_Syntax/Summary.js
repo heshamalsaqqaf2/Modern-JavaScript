d44f9e7f16c8134cf42f5842a0ff99a14f3d4173
Hesham Alsaqqaf <heshamalsaqqaf14@gmail.com>
2025-03-01 01:14:37 +0300

    Add Async/Await Concept.

---
+++ 07_Async_JavaScript/04_Async__Await_Syntax/Summary.js
@@ -0,0 +1,12 @@
+/** 
+ * @summary
+ *     1- Async/await is built on top of Promises
+ *     2- Makes asynchronous code look synchronous
+ *     3- async functions always return Promises
+ *     4- Try/catch blocks work for error handling
+ *     5- Can handle multiple Promises either sequentially or in parallel
+ *     6- Proper error handling is crucial
+ *     7- Independent operations should run in parallel
+ * 
+ *  * Next, we'll look at the Fetch API, where we'll use async/await to make HTTP requests.
+*/
