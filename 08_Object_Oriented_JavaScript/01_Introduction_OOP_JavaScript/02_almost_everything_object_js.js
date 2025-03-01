eb9c070eda3716dd0474812ae19ca8cffc8ecfc3
Hesham Alsaqqaf <heshamalsaqqaf14@gmail.com>
2025-03-01 03:56:39 +0300

    Add OOP Concept.

--- 08_Object_Oriented_JavaScript/01_Introduction_OOP_JavaScript/02_almost_everything_object_js.js
+++ 08_Object_Oriented_JavaScript/01_Introduction_OOP_JavaScript/02_almost_everything_object_js.js
@@ -1,7 +1,6 @@
 'use strict';
 
 // TODO: almost everything you work with is an object. Let's see what this means:
-
 // Numbers have methods - they're objects!
 const num = 42;
 console.log(num.toString()); // "42"
@@ -16,19 +15,22 @@ function greet() {
     console.log('Hi!');
 }
 console.log(typeof greet); // "function"
-console.log(greet instanceof Object); // true
+console.log('Functions Is Object:', greet instanceof Object); // true
 
 // Arrays are objects
 const arr = [1, 2, 3];
-console.log(typeof arr); // "object"
-
-// The only primitives that aren't objects are:
-// - null
-// - undefined
-// - numbers  (they're automatically converted to Number objects when needed)
-// - strings  (they're automatically converted to String objects when needed)
-// - booleans (they're automatically converted to Boolean objects when needed)
-// - symbols
+console.log('Array Type:', typeof arr); // "object"
+/** 
+ * @summary: 
+ *   *The only primitives that aren't objects are:
+ *      - null
+ *      - undefined
+ *      - numbers  (they're automatically converted to Number objects when needed)
+ *      - strings  (they're automatically converted to String objects when needed)
+ *      - booleans (they're automatically converted to Boolean objects when needed)
+ *      - symbols
+ * 
+*/
 
 // JavaScript automatically "wraps" primitives when needed
 const primitive = "hello";
@@ -36,7 +38,6 @@ console.log(primitive.length); // 5 - JavaScript temporarily converts to String 
 
 
 // TODO: This is why you can do things like:
-
 // This works because JavaScript temporarily converts the string to an object
 "hello".toUpperCase();
 
--- 08_Object_Oriented_JavaScript/01_Introduction_OOP_JavaScript/03_understanding_prototypes.js
+++ 08_Object_Oriented_JavaScript/01_Introduction_OOP_JavaScript/03_understanding_prototypes.js
@@ -1,18 +1,18 @@
 'use strict';
 
-const car = {
-    brand: 'Toyota'
-};
+// TODO: ------------ Think of a prototype as a blueprint that contains shared properties and methods ------------
+const car = { brand: 'Toyota', };
 
 // Let's look at car's prototype
 console.log(Object.getPrototypeOf(car));
-/* You'll see something like:
-{
-    constructor: ƒ Object()
-    hasOwnProperty: ƒ hasOwnProperty()
-    toString: ƒ toString()
-    ...and more methods
-}
+/**  
+ * @borrows: You'll see something like browser console:
+    {
+        constructor: ƒ Object()
+        hasOwnProperty: ƒ hasOwnProperty()
+        toString: ƒ toString()
+        ...and more methods
+    }
 */
 
 // This is why we can call methods like toString() on any object
@@ -20,3 +20,12 @@ console.log(car.toString()); // "[object Object]"
 
 // But where did toString() come from? The car object doesn't have it!
 console.log(car.hasOwnProperty('toString')); // false
+
+// TODO: ------------ This is called the "prototype chain". Let's visualize it: ------------
+const myString = "Hello";
+
+// myString's prototype chain:
+// myString -> String.prototype -> Object.prototype -> null
+
+console.log(myString.toUpperCase()); // From String.prototype
+console.log(myString.toString());    // From Object.prototype
